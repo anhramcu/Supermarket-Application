932b60d005c90ff8473aa415ff8cc8cdfa809bc7
anhramcu <87264356+anhramcu@users.noreply.github.com>
2023-10-25 20:12:29 +0700

    Add files via upload

--- Supermarket/src/main/java/Model/Product.java
+++ Supermarket/src/main/java/Model/Product.java
@@ -4,10 +4,42 @@
  */
 package Model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author binh
  */
 public class Product {
+    private int id;
+    public String name;
+    private String describe;
+    public ArrayList<ProductDetail> listProduct = new ArrayList<>();
+    public Product() {
+    }
+
+    public Product(int id, String name, String describe) {
+        this.id = id;
+        this.name = name;
+        this.describe = describe;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescribe() {
+        return describe;
+    }
+
+    public ArrayList<ProductDetail> getListProduct() {
+        return listProduct;
+    }
+    
+    
     
 }
